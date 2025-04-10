e41aa5609eef1ca7228358d5bc9c20a8fdec8b05
Ayush-Vishwakarma <ayush.vishwakarma@shorthills.ai>
2025-04-10 11:17:04 +0530

    basics for scrapping

---
+++ main.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+
+# to get the home.html file -> need to know how to work with files
+
+# open the file with read only format
+with open('home.html', 'r') as html_file:
+    content= html_file.read()
+    # print(content)
+    
+    # create an instance oif beautiful soupo and set the parser and the document to be scrappeed
+    soup=BeautifulSoup(content, 'lxml')
+    #   soup is an instance, content is being parsed using 'lxml' parsing
+    # print(soup.prettify())
+    # now this soup contains the parsed content
+
+    # tags=soup.find('h5')  searched and brings only the first matching tag
+    courses=soup.find_all('h5')
+    print(courses)
+    # since we have a list we need to iterate only each element
+    for course in courses:
+        print(course.text)         # .text gives the text inside the tag
